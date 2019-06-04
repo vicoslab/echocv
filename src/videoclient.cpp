db5cfe40fa97e80ef752a56f607a1023b7fd35e8
Luka Čehovin <luka.cehovin@gmail.com>
2019-06-04 13:30:29 +0200

    Headless videoclient.

--- src/videoclient.cpp
+++ src/videoclient.cpp
@@ -24,6 +24,8 @@ int main(int argc, char** argv) {
     shared_ptr<Frame> frame;
     bool incoming = false;
 
+    bool headless = (NULL == getenv("DISPLAY"));
+
     function<void(shared_ptr<Frame>)> frame_callback = [&](shared_ptr<Frame> m) {
 
         frame = m;
@@ -39,14 +41,21 @@ int main(int argc, char** argv) {
 
             std::time_t tt = std::chrono::system_clock::to_time_t ( frame->header.timestamp );
 
-            cv::putText(frame->image, ctime(&tt), Point(10, 50), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255, 0, 0), 3);
+            
 
-            imshow("EchoCV demo", frame->image);
+            if (!headless) {
+                cv::putText(frame->image, ctime(&tt), Point(10, 50), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255, 0, 0), 3);
+                imshow("EchoCV demo", frame->image);
+            }
+            else {
+                cout << "Frame received, timestamp = " << ctime(&tt) << endl;
+            }
+
             incoming = false;
         }
 
         if (!echolib::wait(20)) break;
-        if (!waitKey(1)) break;
+        if (!waitKey(1) && !headless) break;
     }
 
     exit(0);
